a1738ad8e5981e9da5be9bc6ee0591b1bd06a6d6
Tamarl <tamar.rojas@usach.cm>
2026-06-18 09:48:26 -0400

    Se importó de manera preliminar la base de datos al script y tambien se ropiló papers relacionados a lo que nos corresponde estudiar

--- Scripts/Código.R
+++ Scripts/Código.R
@@ -1,2 +1,22 @@
-1+1 
-print("hola mundo")
+#Cargar paquetes----
+library(readr)
+library(dplyr)
+library(lubridate)
+
+#Rutas y Carga de datos----
+ruta_base <- "BBDD/query.csv"
+
+sismos_raw <- read_csv(ruta_base, show_col_types = FALSE) #Datos crudos
+View(sismos_raw)
+
+# Preparar variables básicas para análisis temporal
+
+sismos <- sismos_raw %>%
+  mutate(
+    fecha_hora_utc = ymd_hms(time, tz = "UTC"),
+    fecha = as.Date(fecha_hora_utc),
+    año = year(fecha_hora_utc),
+    mes = month(fecha_hora_utc)
+  )
+
+View(sismos)
